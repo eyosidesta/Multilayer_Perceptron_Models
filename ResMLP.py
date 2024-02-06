f029dad17e4189c1e01d1c8a4d4fe366d15e1ffb
eyosidesta <mamesmsdesta@gmail.com>
2024-02-05 23:56:34 -0500

    import libraries for ResMLP model

---
+++ ResMLP.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+
