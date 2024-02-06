d1f8f982b113cdba1a38801446eba9034358ca41
eyosidesta <mamesmsdesta@gmail.com>
2024-02-05 23:57:41 -0500

    normalize the training data

--- ResMLP.py
+++ ResMLP.py
@@ -14,4 +14,19 @@ class ResidualMLPClassifier:
         (x_train, y_train), (x_test, y_test) = mnist.load_data()
         return x_train, y_train, x_test, y_test
 
+    def ResMLP_get_input_val(self, x_train):
+        width = x_train.shape[1]
+        height = x_train.shape[2]
+        input_val = width * height
+        return input_val
+
+    def ResMLP_scale_image_size(self, x_train, x_test, input_val):
+        x_train = np.reshape(x_train, [-1, input_val])
+        x_train = x_train.astype('float32') / 255
+
+        x_test = np.reshape(x_test, [-1, input_val])
+        x_test = x_test.astype('float32') / 255
+
+        return x_train, x_test
+
     
