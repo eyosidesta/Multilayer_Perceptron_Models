21d79a5c4494dd266bf7e6e2408ffd9ad2c42710
eyosidesta <mamesmsdesta@gmail.com>
2024-02-05 23:57:06 -0500

    load MNIST dataset

--- ResMLP.py
+++ ResMLP.py
@@ -5,3 +5,13 @@ from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
 
+class ResidualMLPClassifier:
+    def __init__(self):
+        self.model = None
+        self.history = None
+
+    def load_mnist_data(self):
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        return x_train, y_train, x_test, y_test
+
+    
